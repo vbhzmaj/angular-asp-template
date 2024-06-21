eef0a179573d6ac124dbbd6a5e6c6a1bca9c7afb
vbhzmaj <33709353+vbhzmaj@users.noreply.github.com>
2024-06-22 00:42:37 +0200

    create GET method

--- bckndtmp/bckndtmp/Controllers/QuestionsController.cs
+++ bckndtmp/bckndtmp/Controllers/QuestionsController.cs
@@ -7,6 +7,18 @@ namespace bckndtmp.Controllers
     [ApiController]
     public class QuestionsController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            return new Models.Question[] {
+
+                new Models.Question() {QuestionTxt = "Hello"},
+                new Models.Question() {QuestionTxt = "Hi"}
+            };
+        }
+
+
+
         [HttpPost]
         public void Post([FromBody]Models.Question question)
         {
